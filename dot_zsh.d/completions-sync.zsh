2ae282f2a8ca7b016fe3a142f7ba733ead2c2197
Stephan Leicht Vogt <stephan@leichtvogt.ch>
2026-02-16 16:37:19 +0100

    fix(32): add bottom preview-window to fzf-tab global flags

--- dot_zsh.d/completions-sync.zsh
+++ dot_zsh.d/completions-sync.zsh
@@ -43,7 +43,7 @@ zstyle ':completion:*:*:docker-*:*' option-stacking yes
 
 # === fzf-tab ===
 # Switch fzf-tab completion to use tmux popup if available, otherwise standard
-zstyle ':fzf-tab:*' fzf-flags --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9,fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9,info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6,marker:#ff79c6,spinner:#ffb86c,header:#6272a4
+zstyle ':fzf-tab:*' fzf-flags --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9,fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9,info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6,marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --preview-window='bottom,40%,border-rounded'
 # Use the same group colouring for completion groups
 zstyle ':fzf-tab:*' switch-group '<' '>'
 
